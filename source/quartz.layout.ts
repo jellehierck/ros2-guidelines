446aa7ab3ad099af4fccd590d2fb2928de58a06e
Jelle Hierck <j.j.hierck@student.utwente.nl>
2025-05-05 16:40:07 +0200

    Folder index page is the same as content page

--- source/quartz.layout.ts
+++ source/quartz.layout.ts
@@ -36,14 +36,4 @@ export const defaultContentPageLayout: PageLayout = {
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
-export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
-  left: [
-    Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
-    Component.Search(),
-    Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
-  ],
-  right: [],
-}
+export const defaultListPageLayout: PageLayout = defaultContentPageLayout;
